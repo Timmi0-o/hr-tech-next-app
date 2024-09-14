7e416f9d8748abc8e2f6f43adf35e6769a103d72
Timmi0-o <tima53419@gmail.com>
2024-09-14 11:52:42 +0700

    update: new svg + page 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+	return (
+		<div className='w-full bg-[#DAE6F2]'>
+			<h1 className='text-[2.472vw] font-semibold text-center py-[13.889vw] text-[#000000b4]'>
+				Page not found 404
+			</h1>
+		</div>
+	)
+}
